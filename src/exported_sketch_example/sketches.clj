9699989aa4f2cc54de1c3e3a21a856231847ff94
Ben Swift <ben@benswift.me>
2016-10-19 10:37:57 +1100

    add proxy stuff to override exitActual PApplet method

--- src/exported_sketch_example/sketches.clj
+++ src/exported_sketch_example/sketches.clj
@@ -1,22 +1,16 @@
 (ns exported-sketch-example.sketches
   (:import ExampleSketch RandomCircles processing.core.PApplet))
 
+(defmacro create-proxy [sketch-class]
+  `(proxy [~sketch-class] []
+     (exitActual [] (println "exiting... not really."))))
+
 (defn create-all []
-  [{:uid "u1234567" :applet (ExampleSketch.) :weight 1}
-   {:uid "u2345678" :applet (RandomCircles.) :weight 1}])
+  [{:uid "u1234567" :applet (create-proxy ExampleSketch) :weight 1}
+   {:uid "u2345678" :applet (create-proxy RandomCircles) :weight 1}])
 
 (defn run [{:keys [uid applet weight]}]
   (PApplet/runSketch
       (into-array String [uid])
     applet))
 
-(defn testone [{:keys [uid applet weight]}]
-  (println uid applet weight))
-
-(testone (keys (take 1 (create-all))))
-
-(keys (do (take 1 (create-all))))
-
-(keys {:a 1 :b 2})
-
-(type ( (create-all)))

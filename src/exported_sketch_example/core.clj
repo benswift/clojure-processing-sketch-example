bf95e49667eff8af19bd0203319f31c848ad6348
Ben Swift <ben@benswift.me>
2016-10-19 18:03:55 +1100

    works! simplified, no quil

--- src/exported_sketch_example/core.clj
+++ src/exported_sketch_example/core.clj
@@ -1,9 +1,15 @@
 (ns exported-sketch-example.core
-  (:require [quil.core :as q]
-            [exported-sketch-example.sketches :as sk])
+  (:require [exported-sketch-example.sketches :as sk])
   (:gen-class))
 
-(def sketches (sk/create-all))
-
 (defn -main [& args]
-  (sk/run (first (shuffle sketches))))
+  (sk/run-example-sketch)
+  (Thread/sleep 5000)
+  (sk/run-random-circles)
+  (Thread/sleep 5000)
+  (sk/run-example-sketch)
+  (Thread/sleep 5000)
+  (sk/run-random-circles)
+  (Thread/sleep 5000)
+  (println "done, bye!")
+  (System/exit 0))
--- src/exported_sketch_example/sketches.clj
+++ src/exported_sketch_example/sketches.clj
@@ -1,16 +1,46 @@
 (ns exported-sketch-example.sketches
-  (:import ExampleSketch RandomCircles processing.core.PApplet))
+  (:import processing.core.PApplet
+           ;; processing.app.Sketch
+           ;; processing.mode.java.JavaBuild
+           ;; specific sketches
+           ExampleSketch RandomCircles))
 
-(defmacro create-proxy [sketch-class]
-  `(proxy [~sketch-class] []
-     (exitActual [] (println "exiting... not really."))))
-
-(defn create-all []
-  [{:uid "u1234567" :applet (create-proxy ExampleSketch) :weight 1}
-   {:uid "u2345678" :applet (create-proxy RandomCircles) :weight 1}])
-
-(defn run [{:keys [uid applet weight]}]
+(defn run-example-sketch []
   (PApplet/runSketch
-      (into-array String [uid])
-    applet))
+      (into-array String ["mysketch" "--present"])
+      (proxy [ExampleSketch] []
+        (exitActual []
+          (println "seeya mate!")
+          (proxy-super destroy)))))
 
+(defn run-random-circles []
+  (PApplet/runSketch
+      (into-array String ["mysketch" "--present"])
+      (proxy [RandomCircles] []
+        (exitActual []
+          (println "seeya mate!")
+          (proxy-super destroy)))))
+
+;; ;; what should the Mode be?
+;; ;; (defn export-java [folder]
+;; ;;   (.preprocess (JavaBuild. (Sketch. mode))))
+
+;; (defmacro create-proxy [sketch-class]
+;;   `(proxy [~sketch-class] []
+;;      (handleKeyEvent [event]
+;;        (println "keyPressed")
+;;        ;; (when (= (.getKeyCode event) 0)
+;;        ;;   (.setKeyCode event 0))
+;;        ;; (proxy-super event)
+;;        )
+;;      (exitActual []
+;;        (println "exiting... not really."))))
+
+;; (defn create-all []
+;;   [{:uid "u1234567" :applet (create-proxy ExampleSketch) :weight 1}
+;;    {:uid "u2345678" :applet (create-proxy RandomCircles) :weight 1}])
+
+;; (defn run [{:keys [uid applet weight]}]
+;;   (PApplet/runSketch
+;;       (into-array String [uid "--present"])
+;;     applet))

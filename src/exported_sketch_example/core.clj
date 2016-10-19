970ca4b85c24b3ca8ccd1c7390aa50a7832def6d
Kieran Browne <kierantbrowne@gmail.com>
2016-10-20 00:35:47 +1100

    run-sketch macro

--- src/exported_sketch_example/core.clj
+++ src/exported_sketch_example/core.clj
@@ -1,16 +1,20 @@
 (ns exported-sketch-example.core
+  (:import ExampleSketch RandomCircles)
   (:require [exported-sketch-example.sketches :as sk])
   (:gen-class))
 
+(def sketches [ExampleSketch RandomCircles])
+(defmacro rand-sketch []
+  (rand-nth sketches))
+
 (defn -main [& args]
   (let [sleep-dur 5000]
-    (sk/run-example-sketch)
+    ; specific sketches can be run as an argument run-sketch 
+    ; ((double parentheses due to macro))
+    ((sk/run-sketch ExampleSketch))
     (Thread/sleep sleep-dur)
-    (sk/run-random-circles)
-    (Thread/sleep sleep-dur)
-    (sk/run-example-sketch)
-    (Thread/sleep sleep-dur)
-    (sk/run-random-circles)
+    ((sk/run-sketch RandomCircles))
+    ; but we'll probably want to use a vector to store them
     (Thread/sleep sleep-dur))
   (println "done, bye!")
   (System/exit 0))
--- src/exported_sketch_example/sketches.clj
+++ src/exported_sketch_example/sketches.clj
@@ -1,4 +1,5 @@
 (ns exported-sketch-example.sketches
+  (:use clojure.walk)
   (:import processing.core.PApplet
            ;; specific sketches
            ExampleSketch RandomCircles))
@@ -6,18 +7,12 @@
 ;; obviously, having a separate method for each sketch class doesn't
 ;; really scale. Macros to the rescue?
 
-(defn run-example-sketch []
-  (PApplet/runSketch
-      (into-array String ["mysketch" "--present"])
-      (proxy [ExampleSketch] []
-        (exitActual []
-          (println "seeya mate!")
-          (proxy-super destroy)))))
+(defmacro run-sketch
+  [sketch]
+  `(fn [] 
+     (PApplet/runSketch 
+       (into-array String ["mysketch" "--present"])
+       (proxy [~sketch] []
+         (exitActual []
+           (proxy-super destroy))))))
 
-(defn run-random-circles []
-  (PApplet/runSketch
-      (into-array String ["mysketch" "--present"])
-      (proxy [RandomCircles] []
-        (exitActual []
-          (println "seeya mate!")
-          (proxy-super destroy)))))

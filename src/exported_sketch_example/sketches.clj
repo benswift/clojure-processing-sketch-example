236434196929f0944f3add5aed4d4035f1e88991
Ben Swift <ben@benswift.me>
2016-10-19 18:05:08 +1100

    remove some unused guff

--- src/exported_sketch_example/sketches.clj
+++ src/exported_sketch_example/sketches.clj
@@ -1,9 +1,10 @@
 (ns exported-sketch-example.sketches
   (:import processing.core.PApplet
-           ;; processing.app.Sketch
-           ;; processing.mode.java.JavaBuild
            ;; specific sketches
            ExampleSketch RandomCircles))
+
+;; obviously, having a separate method for each sketch class doesn't
+;; really scale. Macros to the rescue?
 
 (defn run-example-sketch []
   (PApplet/runSketch
@@ -20,27 +21,3 @@
         (exitActual []
           (println "seeya mate!")
           (proxy-super destroy)))))
-
-;; ;; what should the Mode be?
-;; ;; (defn export-java [folder]
-;; ;;   (.preprocess (JavaBuild. (Sketch. mode))))
-
-;; (defmacro create-proxy [sketch-class]
-;;   `(proxy [~sketch-class] []
-;;      (handleKeyEvent [event]
-;;        (println "keyPressed")
-;;        ;; (when (= (.getKeyCode event) 0)
-;;        ;;   (.setKeyCode event 0))
-;;        ;; (proxy-super event)
-;;        )
-;;      (exitActual []
-;;        (println "exiting... not really."))))
-
-;; (defn create-all []
-;;   [{:uid "u1234567" :applet (create-proxy ExampleSketch) :weight 1}
-;;    {:uid "u2345678" :applet (create-proxy RandomCircles) :weight 1}])
-
-;; (defn run [{:keys [uid applet weight]}]
-;;   (PApplet/runSketch
-;;       (into-array String [uid "--present"])
-;;     applet))

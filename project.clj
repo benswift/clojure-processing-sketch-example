23953c3acfa32a97085808dc061295de67fe7d28
Ben Swift <ben@benswift.me>
2016-10-23 16:05:39 +1100

    add comment with link to cli opts parser

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.processing/core "3.2.1"]
-                 [ddf.minim "2.2.0"]]
+                 [ddf.minim "2.2.0"]
+                 ;; probably want this too - https://github.com/clojure/tools.cli
+                 ]
   :java-source-paths ["src/sketches"]
   :plugins [[lein-localrepo "0.5.3"]])

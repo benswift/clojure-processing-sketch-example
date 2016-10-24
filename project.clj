df532a0d74f4c159b4570d89aadc13e943a89974
Ben Swift <ben@benswift.me>
2016-10-24 17:32:24 +1100

    use lein-localrepo instead of compiling java files
    big refactor - still having problems with the data path, though

--- project.clj
+++ project.clj
@@ -6,7 +6,16 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.processing/core "3.2.1"]
-                 [ddf.minim "2.2.0"]
+                 [comp1720/tritonus_share "0.0.0-SNAPSHOT"]
+                 [comp1720/jsminim "0.0.0-SNAPSHOT"]
+                 [comp1720/u3333333 "0.0.0-SNAPSHOT"]
+                 [comp1720/u2222222 "0.0.0-SNAPSHOT"]
+                 [comp1720/Jukebox "0.0.0-SNAPSHOT"]
+                 [comp1720/u1111111 "0.0.0-SNAPSHOT"]
+                 [comp1720/minim "0.0.0-SNAPSHOT"]
+                 [comp1720/mp3spi1.9.5 "0.0.0-SNAPSHOT"]
+                 [comp1720/jl1.0.1 "0.0.0-SNAPSHOT"]
+                 [comp1720/tritonus_aos "0.0.0-SNAPSHOT"]
                  ;; probably want this too - https://github.com/clojure/tools.cli
                  ]
   :java-source-paths ["src/sketches"]
--- utils/exporter.el
+++ utils/exporter.el
@@ -1,17 +1,17 @@
 (require 'dash)
 
-(defun export-sketches (source-dir dest-dir)
+(defun export-sketches (source-dir)
   (-each
       (directory-files source-dir :full "^[^.].*[^.]$")
     (lambda (fname)
-      (let ((app-path (format "%s/application.macosx" fname)))
+      (let* ((app-path (format "%s/application.macosx" fname))
+             (command (format "/usr/local/bin/processing-java --force --sketch=%s --export --platform=macosx --bits=64 --output=%s/export"
+                              fname
+                              fname)))
         (when (file-directory-p app-path)
           (delete-directory app-path :recursive))
-        (shell-command
-         (format "/usr/local/bin/processing-java --force --sketch=%s --export"
-                 fname))))))
-
-(export-sketches "/Users/ben/Code/clojure/clojure-processing-sketch-example/processing" "/Users/ben/Code/clojure/clojure-processing-sketch-example/src/sketches")
+        (message command)
+        (shell-command command)))))
 
 (defun find-jars (source-dir)
   (-uniq
@@ -27,17 +27,23 @@
                                     "jogl-all.jar")))))
        (directory-files-recursively sketch-folder "jar$")))
    
-    (directory-files source-dir "^u[0-9]\\{7\\}$"))))
+    (directory-files source-dir "^\\(u[0-9]\\{7\\}\\|Jukebox\\)$"))))
 
 (defun install-jar (jar-path)
-  (shell-command (format "lein localrepo install %s comp1720/%s 0.0.0-SNAPSHOT"
+  (let ((command (format "lein localrepo install %s comp1720/%s 0.0.0-SNAPSHOT"
                          jar-path
                          (file-name-base jar-path))))
+    (message command)
+    (shell-command command)))
 
 (defun install-all-jars (source-dir)
   (-each (find-jars source-dir) #'install-jar))
 
-(install-all-jars "/Users/ben/Code/clojure/clojure-processing-sketch-example/processing")
+(defun export-and-install (source-dir)
+  (export-sketches source-dir)
+  (install-all-jars source-dir))
+
+(export-and-install "/Users/ben/Code/clojure/clojure-processing-sketch-example/processing")
 
 ;; this currently doesn't work - should point to the data folders next
 ;; to the .pde files, not in the src/sketches folder

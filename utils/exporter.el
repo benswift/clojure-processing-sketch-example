719aa7bcd0d130a8cabc6e24bf3e5aacd9388bf5
Ben Swift <ben@benswift.me>
2016-10-24 17:03:46 +1100

    add install-all-jars elisp

--- utils/exporter.el
+++ utils/exporter.el
@@ -17,20 +17,34 @@
 
 (require 'dash)
 
-(defun process-jars (source-dir)
-  (let ((filter-fn
-         (lambda (s) (and (not (s-contains? "/jre/" s))
-                     (not (member (file-name-nondirectory s)
-                              '("core.jar"
-                                "gluegen-rt-natives-macosx-universal.jar"
-                                "gluegen-rt.jar"
-                                "jogl-all-natives-macosx-universal.jar"
-                                "jogl-all.jar")))))))
-    (-filter filter-fn
-             (directory-files-recursively source-dir "jar$"))))
+(defun find-jars (source-dir)
+  (-uniq
+   (-mapcat
+    (lambda (sketch-folder)
+      (-filter
+       (lambda (jar) (and (not (s-contains? "/jre/" jar))
+                     (not (member (file-name-nondirectory jar)
+                                  '("core.jar"
+                                    "gluegen-rt-natives-macosx-universal.jar"
+                                    "gluegen-rt.jar"
+                                    "jogl-all-natives-macosx-universal.jar"
+                                    "jogl-all.jar")))))
+       (directory-files-recursively sketch-folder "jar$")))
+   
+    (directory-files source-dir "^u[0-9]\\{7\\}$"))))
 
-(-each (process-jars "/Users/ben/Code/clojure/clojure-processing-sketch-example/processing") #'message)
+(defun install-jar (jar-path)
+  (shell-command (format "lein localrepo install %s comp1720/%s 0.0.0-SNAPSHOT"
+                         jar-path
+                         (file-name-base jar-path))))
 
+(defun install-all-jars (source-dir)
+  (-each (find-jars source-dir) #'install-jar))
+
+(install-all-jars "/Users/ben/Code/clojure/clojure-processing-sketch-example/processing")
+
+;; this currently doesn't work - should point to the data folders next
+;; to the .pde files, not in the src/sketches folder
 (defun munge-data-folder-paths (directory)
   (-each
       (directory-files directory :full "java$")

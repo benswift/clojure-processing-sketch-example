12aa86da8b28c985b3c247ed9f65085236220a94
Ben Swift <ben@benswift.me>
2016-10-26 17:22:48 +1100

    remove papplet stuff, use tmux

--- src/clojure_processing_sketch_example/sketches.clj
+++ src/clojure_processing_sketch_example/sketches.clj
@@ -8,9 +8,6 @@
 (def current-sketch
   (atom {}))
 
-(defn current-papplet []
-  (:papplet @current-sketch))
-
 (defn current-idle-time []
   (/ (- (System/currentTimeMillis) (:time @current-sketch)) 1000.0))
 
@@ -25,25 +22,20 @@
       papplet)
   papplet)
 
-(defn exit-papplet
-  ([papplet]
-   (.exit papplet))
-  ([papplet delay-ms]
-   (Thread/sleep delay-ms)
-   ;; probably should check it hasn't already been exited before
-   ;; calling exit?
-   (.exit papplet)))
-
 (defn sketch-for-uid [uid]
   (first (filter  #(= (:uid %) uid) @sketches)))
+
+(defn kill-current []
+  (sh "killall" "tmux"))
 
 (defn start
   ([]
    (start (->> @sketches (map :uid) rand-nth)))
   ([uid]
+   (sh "tmux" "new" "-ds" "current-sketch" "processing-java" (str "--sketch=" (System/getProperty "user.dir") "/processing/" uid))
    (reset!
     current-sketch
-    {:papplet (run-papplet ((:constructor (sketch-for-uid uid))))
+    {:uid uid
      :time (System/currentTimeMillis)})))
 
 (defn jukebox-proxy []
@@ -63,46 +55,22 @@
 
 (defn start-jukebox
   ([]
-   (reset!
-    current-sketch
-    {:papplet (run-papplet ((jukebox-proxy)))
-     :time (System/currentTimeMillis)})))
-
-(def juke ((jukebox-proxy)))
-
-(defmacro sketch-proxy
-  [papplet-subclass]
-  (let [event-sym (gensym "event")]
-    `(fn []
-       (proxy [~papplet-subclass] []
-         (exitActual []
-           (println "sketch recieved exitActual")
-           (start-jukebox))
-         (handleKeyEvent [~event-sym]
-           (touch-current-time)
-           (proxy-super handleKeyEvent ~event-sym))
-         (handleMouseEvent [~event-sym]
-           (touch-current-time)
-           (proxy-super handleMouseEvent ~event-sym))))))
+   ((jukebox-proxy))
+   (swap! current-sketch assoc :time (System/currentTimeMillis))))
 
 (defn gallery-loop [idle-time]
   (loop []
     (when (> (current-idle-time) idle-time)
-      (.exit (current-papplet))
-      (if (instance? Jukebox 
-                     (current-papplet))
-        (start)
-        (do
-          (.exit (current-papplet))
-          (start-jukebox))))
-    (Thread/sleep 1000)
+      (kill-current)
+      (start))
+    (Thread/sleep 10000)
     (recur)))
 
 (defn init []
   (reset! sketches
-          [{:uid "u1111111" :constructor (sketch-proxy u1111111) :weight 1}
-           {:uid "u2222222" :constructor (sketch-proxy u2222222) :weight 1}
-           ;; {:uid "u3333333" :constructor (sketch-proxy u3333333) :weight 1}
-           ;; {:uid "u4444444" :constructor (sketch-proxy u4444444) :weight 1}
-           ])
+          [{:uid "u1111111" :weight 1}
+           {:uid "u3333333" :weight 1}
+           {:uid "u2222222" :weight 1}
+           {:uid "u4444444" :weight 1}])
+  (reset! current-sketch {:uid nil :time (System/currentTimeMillis)})
   (start-jukebox))

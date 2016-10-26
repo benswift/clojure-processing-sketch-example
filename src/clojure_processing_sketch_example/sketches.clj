4c6c6feb656269a33a3e731416f32e3447eb2bad
Ben Swift <ben@benswift.me>
2016-10-26 11:37:37 +1100

    refactor sketches (handles switchToSketch())

--- src/clojure_processing_sketch_example/sketches.clj
+++ src/clojure_processing_sketch_example/sketches.clj
@@ -4,9 +4,12 @@
            Jukebox u1111111 u2222222 u3333333 u4444444))
 
 ;; keys should be [papplet time]
+
+(def sketches
+  (atom []))
+
 (def current-sketch
-  (atom {:papplet nil
-         :time (System/currentTimeMillis)}))
+  (atom {}))
 
 (defn current-papplet []
   (:papplet @current-sketch))
@@ -39,11 +42,28 @@
   (if (and papplet (.isLooping papplet))
     (exit-applet papplet)))
 
+(defn sketch-for-uid [uid]
+  (first (filter  #(= (:uid %) uid) @sketches)))
+
+(defn start
+  ([]
+   (start (->> @sketches (map :uid) rand-nth)))
+  ([uid]
+   (let [old-papplet (current-papplet)
+         new-papplet ((:constructor (sketch-for-uid uid)))]
+     (and old-papplet (exit-applet old-papplet))
+     (run-applet new-papplet)
+     (reset!
+      current-sketch
+      {:papplet new-papplet
+       :time (System/currentTimeMillis)}))))
+
 (defn jukebox-proxy []
   (fn []
     (proxy [Jukebox] []
       (exitActual [])
       (switchToSketch [uid]
+        (println "switching to " uid)
         (start uid)))))
 
 (defn start-jukebox
@@ -71,35 +91,19 @@
            (touch-current-time)
            (proxy-super handleMouseEvent ~event-sym))))))
 
-(def sketches
-  (atom []))
-[{:uid "u1111111" :constructor (sketch-proxy u1111111) :weight 1}
- {:uid "u2222222" :constructor (sketch-proxy u2222222) :weight 1}
- {:uid "u3333333" :constructor (sketch-proxy u3333333) :weight 1}
- {:uid "u4444444" :constructor (sketch-proxy u4444444) :weight 1}]
-
-(def init-sketches
-  (reset! sketches))
-
-(defn start
-  ([]
-   (start (->> sketches (map :uid) rand-nth)))
-  ([uid]
-   (let [old-papplet (current-papplet)
-         new-papplet ((:constructor (sketch-for-uid uid)))]
-     (and old-papplet (exit-applet old-papplet))
-     (run-applet new-papplet)
-     (reset!
-      current-sketch
-      {:papplet new-papplet
-       :time (System/currentTimeMillis)}))))
-
-(defn sketch-for-uid [uid]
-  (first (filter  #(= (:uid %) uid) sketches)))
-
 (defn gallery-loop [idle-time]
   (loop []
     (if (> (current-idle-time) idle-time)
       (start))
     (Thread/sleep 1000)
     (recur)))
+
+(defn reset-sketches []
+  (reset! sketches
+          [{:uid "u1111111" :constructor (sketch-proxy u1111111) :weight 1}
+           {:uid "u2222222" :constructor (sketch-proxy u2222222) :weight 1}
+           {:uid "u3333333" :constructor (sketch-proxy u3333333) :weight 1}
+           {:uid "u4444444" :constructor (sketch-proxy u4444444) :weight 1}])
+  (reset! current-sketch
+          {:papplet nil
+           :time (System/currentTimeMillis)}))
